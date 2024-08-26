78b1fe3358f9985743b9dfba120eee363221121e
Adnan <adnancpanakkad@gmail.com>
2024-08-26 16:02:21 +0530

    try_catch

--- PRACTICE.dart
+++ PRACTICE.dart
@@ -1,7 +1,50 @@
-void main() {
-  List arr = [1, 2, 3, 4, 5];
+class Node {
+  String? data;
+  Node? next;
+  Node? prev;
+  Node(this.data);
+}
 
-  arr.removeWhere((arr) => arr == 1);
-  print(arr.runtimeType);
-  print(arr);
+class LinkidList {
+  Node? head;
+  Node? tail;
+  addValue(String data) {
+    Node newnode = Node(data);
+    if (head == null) {
+      head = newnode;
+    } else {
+      tail?.next = newnode;
+      newnode.prev = tail;
+    }
+    tail = newnode;
+  }
+
+  printlist() {
+    var curr = head;
+    while (curr != null) {
+      print(curr.data);
+      curr = curr.next;
+    }
+  }
+
+  reverlist() {
+    String str = '';
+    var curr = tail;
+    while (curr != null) {
+      str += curr.data.toString();
+      curr = curr.prev;
+    }
+    return str;
+  }
+}
+
+void main() {
+  LinkidList list = LinkidList();
+  String str = 'HELLO WORLD';
+  for (var i = 0; i < str.length; i++) {
+    list.addValue(str[i]);
+  }
+  list.printlist();
+  print('reversed');
+  print(list.reverlist());
 }
--- null_operators.dart/null_aware_cascadingoperator.dart
+++ null_operators.dart/null_aware_cascadingoperator.dart
@@ -1,7 +1,9 @@
 void main() {
+
   String? name = null;
 
   // Using ?? to provide a default value
   String displayName = name ?? 'Guest';
   print(displayName); // Output: Guest
+  
 }
