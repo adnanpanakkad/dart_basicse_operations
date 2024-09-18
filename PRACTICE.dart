83fa4c963e8b8966d2f4697b2a5f47dcb582203e
Adnan <adnancpanakkad@gmail.com>
2024-09-18 15:26:52 +0530

    valid_parasinthesis

--- PRACTICE.dart
+++ PRACTICE.dart
@@ -1,18 +1,7 @@
-bool isPrime(int num, int div) {
-  if (num <= 1) {
-    return false;
-  }
-  if (div == 1) {
-    return true;
-  }
-  if (num % div == 0) {
-    return false;
-  }
-  return isPrime(num, div - 1);
+int FUnction(int a, {required int b, int c = 10}) {
+  return a + b + c;
 }
 
 void main() {
-  int num = 17;
-  bool result = isPrime(num, num ~/ 2);
-  print(result);
+  print(FUnction(12, b: 2));
 }
