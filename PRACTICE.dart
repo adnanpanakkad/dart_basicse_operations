0ed2b1918b5cc7169967b13a884e1e22e3bb1fa7
Adnan <adnancpanakkad@gmail.com>
2024-09-24 15:13:00 +0530

    height of the BST

--- PRACTICE.dart
+++ PRACTICE.dart
@@ -1,10 +1,30 @@
-List<String> findFruitsWithRepeatingSubstrings(List<String> fruits) {
+void main() {
+  List<String> fruits = [
+    'Apple',
+    'Banana',
+    'Orange',
+    'Mango',
+    'Strawberry',
+    'Blueberry',
+    'Pineapple',
+    'Grapes',
+    'Kiwi',
+    'Watermelon'
+  ];
+
+  List<String> result = findFruitsWithRepeatingAlphabets(fruits);
+  print(
+      result); // [Apple, Banana, Strawberry, Blueberry, Pineapple, Kiwi, Watermelon]
+}
+
+List<String> findFruitsWithRepeatingAlphabets(List<String> fruits) {
   List<String> store = [];
+
   for (int i = 0; i < fruits.length; i++) {
     List<String> fruit = fruits[i].split('').toList();
     bool val = true;
-    for (int i = 0; i < fruit.length - 1; i++) {
-      for (int j = 0; j < fruit.length - 1; j++) {
+    for (int i = 0; i < fruit.length; i++) {
+      for (int j = 0; j < fruit.length; j++) {
         if (i != j && fruit[i] == fruit[j]) {
           val = false;
         }
@@ -15,32 +35,4 @@ List<String> findFruitsWithRepeatingSubstrings(List<String> fruits) {
     }
   }
   return store;
-}
-
-void main() {
-  List<String> fruits = [
-    'apple',
-    'banana',
-    'cherry',
-    'date',
-    'elderberry',
-    'fig',
-    'grape',
-    'honeydew',
-    'kiwi',
-    'lemon',
-    'mango',
-    'nectarine',
-    'orange',
-    'papaya',
-    'pineapple',
-    'quince',
-    'raspberry',
-    'strawberry',
-    'tangerine',
-    'watermelon'
-  ];
-
-  List result = findFruitsWithRepeatingSubstrings(fruits);
-  print(result);
 }
