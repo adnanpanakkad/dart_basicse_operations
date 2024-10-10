80eed5d995576969e76a2ddb284e684c775fd6a3
Adnan <adnancpanakkad@gmail.com>
2024-10-10 11:35:20 +0530

    modulo operator

--- PRACTICE.dart
+++ PRACTICE.dart
@@ -1,9 +1,5 @@
 void main() {
-  List<int> arr1 = [1, 2, 35, 7, 9];
-  List<int> arr2 = [3, 4, 5, 7, 9];
-
-  // Convert both lists to sets and find the intersection
-  List<int> duplicates = arr1.toSet().intersection(arr2.toSet()).toList();
-
-  print(duplicates);
+  int num = 5;
+  int result = num % 2;
+  print(result);
 }
--- problems/asending_names.dart
+++ problems/asending_names.dart
@@ -2,7 +2,7 @@ void main() {
   List<String> names = ['Adnan', 'Rasha', 'Maya', 'Haneen','salman'];
 
   // Sort in descending order
-  names.sort((a, b) => b.compareTo(a));
+  names.sort((a, b) => a.compareTo(b));
 
   print(names);
 }
