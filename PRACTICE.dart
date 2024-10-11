aab7defd7503cf24f052d35f0ffee69654607be9
Adnan <adnancpanakkad@gmail.com>
2024-10-11 18:09:41 +0530

    string manipulation

--- PRACTICE.dart
+++ PRACTICE.dart
@@ -1,5 +1,36 @@
+class Node {
+  Map<String, Node> children = {};
+  bool isword = false;
+}
+
+class Trie {
+  Node root = Node();
+  insert(String word) {
+    Node? node = root;
+    for (int i = 0; i < word.length; i++) {
+      var char = word[i];
+      if (!node!.children.containsKey(char)) {
+        node.children[char] = Node();
+      }
+      node = node.children[char];
+    }
+    node!.isword = true;
+  }
+
+  search(String str) {
+    Node node = root;
+    for (int i = 0; i < str.length; i++) {
+      if (!node.children.containsKey(str[i])) {
+        return false;
+      }
+      node = node.children[str[i]]!;
+    }
+    return node.isword;
+  }
+}
+
 void main() {
-  int num = 5;
-  int result = num % 2;
-  print(result);
+  Trie trie = Trie();
+  trie.insert('mam');
+  print(trie.search('mam'));
 }
--- problems/asending_names.dart
+++ problems/asending_names.dart
@@ -2,7 +2,7 @@ void main() {
   List<String> names = ['Adnan', 'Rasha', 'Maya', 'Haneen','salman'];
 
   // Sort in descending order
-  names.sort((a, b) => a.compareTo(b));
+  names.sort((a, b) => b.compareTo(a));
 
   print(names);
 }
