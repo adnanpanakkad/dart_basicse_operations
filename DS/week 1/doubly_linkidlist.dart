15b1a93457d35ba28ead417b90767d9eb4a05e7b
Adnan <adnancpanakkad@gmail.com>
2024-10-16 12:49:49 +0530

    (method):delete node from Linkidlist+

--- DS/week 1/doubly_linkidlist.dart
+++ DS/week 1/doubly_linkidlist.dart
@@ -20,6 +20,48 @@ class DoublyLinkedList {
     tail = newNode;
   }
 
+  void deleteNode(int data) {
+    if (head == null) {
+      print("List is empty");
+      return;
+    }
+
+    Node? curr = head;
+
+    // Case 1: Node to be deleted is the head node
+    if (curr != null && curr.data == data) {
+      head = curr.next; // Move head to the next node
+      if (head != null) {
+        head?.prev = null; // Set the previous pointer of the new head to null
+      } else {
+        tail = null; // If the list becomes empty, set tail to null
+      }
+      return;
+    }
+
+    // Search for the node to be deleted
+    while (curr != null && curr.data != data) {
+      curr = curr.next;
+    }
+
+    // Case 2: Node not found in the list
+    if (curr == null) {
+      print("Node with value $data not found in the list.");
+      return;
+    }
+
+    // Case 3: Node to be deleted is the tail node
+    if (curr == tail) {
+      tail = curr.prev; // Move tail to the previous node
+      tail?.next = null; // Set the next pointer of the new tail to null
+      return;
+    }
+
+    // Case 4: Node to be deleted is in the middle
+    curr.prev?.next = curr.next;
+    curr.next?.prev = curr.prev;
+  }
+
   void printList() {
     var curr = head;
     while (curr != null) {
