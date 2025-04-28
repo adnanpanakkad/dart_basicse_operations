c6b9f515f0a8bd8f7ed6c08ec71aed4fdb4f0fc1
Adnan <adnancpanakkad@gmail.com>
2025-04-28 14:31:51 +0530

    prtctc

--- PRACTICE.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  List<int> arr = [1, 2, 3, 4, 5, 6]; // Specify type for better safety
-  for (int i = 0; i < arr.length - 1; i++) { // Use < arr.length - 1
-    if (arr[i + 1] != 0 && arr[i] % arr[i + 1] == 0) { // Avoid division by zero
-      print(arr[i]);
-    }
-  }
-}
